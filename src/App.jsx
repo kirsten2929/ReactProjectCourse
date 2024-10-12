46193a9a96c47d23ec5419f52abaf5bef8590b87
David <daviddistrajobautista@gmail.com>
2024-10-12 14:56:21 +0800

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Accordion from "./components/accordion/Accordion"
+
+function App() {
+
+  return (
+    <>
+      <Accordion />
+
+    </>
+  )
+}
+
+export default App
---
+++ src/components/accordion/Accordion.jsx
@@ -0,0 +1,13 @@
+
+
+const Accordion = () => {
+    return (
+        <div className="h-screen flex items-center justify-center">
+            <h2 className="bg-yellow-800 text-white font-bold text-lg border-2 border-yellow-900 py-2 px-5">
+                Enable Multi Selection
+            </h2>
+        </div>
+    )
+}
+
+export default Accordion
