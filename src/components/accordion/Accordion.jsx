9105fed4153b68a80ac5e1acceca97213d851da2
David <daviddistrajobautista@gmail.com>
2024-10-14 00:47:15 +0800

    single  selected accordion and toggle button

--- src/components/accordion/Accordion.jsx
+++ src/components/accordion/Accordion.jsx
@@ -1,11 +1,69 @@
-
+import { useState } from "react";
+import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
+import data from "./data";
 
 const Accordion = () => {
+    const [selected, setSelected] = useState(null);
+    const [isOn, setIsOn] = useState(false);
+    function handleSingleSelection(getCurrentId) {
+        setSelected(prevId => (prevId === getCurrentId ? null : getCurrentId));
+    }
+    const handleToggle = () => {
+        setIsOn(!isOn);
+    };
+
     return (
-        <div className="h-screen flex items-center justify-center">
-            <h2 className="bg-yellow-800 text-white font-bold text-lg border-2 border-yellow-900 py-2 px-5">
-                Enable Multi Selection
-            </h2>
+        <div className="h-screen flex flex-col items-center justify-center bg-zinc-100 ">
+            <div className="w-2/4 mb-4 flex flex-row items-center justify-end">
+                <p className="text-xl mx-2">
+                    Enable Multi Selection :
+                </p>
+                <span
+                    onClick={handleToggle}
+                    className={`w-12 h-6 flex items-center bg-gray-300 rounded-full p-1 cursor-pointer
+                        ${isOn ? 'bg-blue-500' : 'bg-gray-300'}`}
+                >
+                    {/* Circle that switches between positions */}
+                    <div
+                        className={`bg-white w-5 h-5 rounded-full shadow-md transform duration-300 ease-in-out
+                             ${isOn ? 'translate-x-6' : ''}`}
+                    ></div>
+                </span>
+            </div>
+
+            <div className="w-2/4">
+                {
+                    data && data.length > 0 ? (
+                        data.map((dataItem) => (
+                            <div
+                                key={dataItem.id}
+                                onClick={() => handleSingleSelection(dataItem.id)}
+                                className="w-full shadow-md border-2">
+                                <div className="w-full p-4 flex flex-col">
+                                    <div className="w-full flex items-center justify-between">
+                                        <h3 className="font-semibold">{dataItem.question}</h3>
+                                        <span>
+                                            {selected === dataItem.id ?
+                                                (<div>
+                                                    <AiOutlineMinus />
+                                                </div>) : <AiOutlinePlus />
+                                            }
+                                        </span>
+                                    </div>
+                                    {selected === dataItem.id ?
+                                        (<div className="mt-4 ">
+                                            {dataItem.answer}
+                                        </div>) : null
+                                    }
+
+                                </div>
+                            </div>
+                        ))
+                    ) : (
+                        <p>No data found</p>
+                    )
+                }
+            </div>
         </div>
     )
 }
